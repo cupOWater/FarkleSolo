f1c64c9553658356fbd5cd8f1d4040c15b20f755
cupOWater <something2805@gmail.com>
2023-08-11 15:18:38 +0700

    Initial Commit

---
+++ FarkleSolo/FarkleSoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarkleSoloApp.swift
+//  FarkleSolo
+//
+//  Created by MacNCheese on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FarkleSoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
